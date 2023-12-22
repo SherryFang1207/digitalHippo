827cb62bb8aa12c4883f45bae44f5d6d44c9c7b0
SherryFang1207 <zfang1207@gmail.com>
2023-12-22 15:28:29 -0800

    setup layout files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import MaxWidthWrapper from "@/components/layout/MaxWidthWrapper";
+
 export default function Home() {
-  return <p>Hello World!</p>;
+  return (
+    <MaxWidthWrapper>
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+        <h1>
+          Your marketplace for high-quality
+          <span className="text-blue-600">digital assets</span>
+        </h1>
+      </div>
+    </MaxWidthWrapper>
+  );
 }
